a37343a2df61200a1394d3a6952a8f47f7f3e518
razhayat <razh.graffiti@gmail.com>
2026-02-21 17:04:54 +0200

    Implemented Unbind and its tests

--- src/union.ts
+++ src/union.ts
@@ -5,3 +5,5 @@ export type UnionKey<T> = T extends T ? keyof T : never;
 export type Normalize<T, K extends UnionKey<T> = UnionKey<T>> = T extends T
 	? Prettify<T & PartialRecord<Exclude<K, keyof T>, never>>
 	: never;
+
+export type Unbind<T> = Omit<Normalize<T>, never>;
