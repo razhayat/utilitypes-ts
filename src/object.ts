3b55217886c7402362d59666431cb29bf5b45829
razhayat <razh.graffiti@gmail.com>
2026-02-21 15:16:30 +0200

    Implemented StrictOmit and its tests

--- src/object.ts
+++ src/object.ts
@@ -1,1 +1,7 @@
+import { UnionKey } from "./union";
+
+export type StrictOmit<T, K extends UnionKey<T>> = T extends T
+	? Omit<T, K>
+	: never;
+
 export type PartialRecord<K extends PropertyKey, V> = Partial<Record<K, V>>;
