cae00aba62ffbee7dba8b956eda24be5ab417973
razhayat <razh.graffiti@gmail.com>
2026-02-21 15:21:52 +0200

    Implemented StrictPick and its tests

--- src/object.ts
+++ src/object.ts
@@ -4,4 +4,8 @@ export type StrictOmit<T, K extends UnionKey<T>> = T extends T
 	? Omit<T, K>
 	: never;
 
+export type StrictPick<T, K extends UnionKey<T>> = T extends T
+	? Pick<T, Extract<K, keyof T>>
+	: never;
+
 export type PartialRecord<K extends PropertyKey, V> = Partial<Record<K, V>>;
