f528f0f6832e2217c9d5103824b635a65ea85429
razhayat <razh.graffiti@gmail.com>
2026-03-22 15:38:36 +0200

    Added more tests to normalize

--- tests/normalize.test-d.ts
+++ tests/normalize.test-d.ts
@@ -132,8 +132,99 @@ describe("Normalize", () => {
 		expectTypeOf<Result>().toEqualTypeOf<ExpectedA | ExpectedB>();
 	});
 
+	it("should work with primitives and objects", () => {
+		type Primitive = "hello";
+		type Obj1 = {
+			hi: string;
+		};
+		type Obj2 = {
+			bye: number;
+		};
+
+		type Result = Normalize<Primitive | Obj1 | Obj2>;
+
+		type ExpectedPrimitive = "hello" & {
+			hi?: never;
+			bye?: never;
+		};
+		type ExpectedObj1 = {
+			hi: string;
+		} & Partial<Record<keyof string | "bye", never>>;
+		type ExpectedObj2 = {
+			bye: number;
+		} & Partial<Record<keyof string | "hi", never>>;
+
+		expectTypeOf<Result>().toEqualTypeOf<
+			ExpectedPrimitive | ExpectedObj1 | ExpectedObj2
+		>();
+	});
+
+	it("should work with null or undefined ", () => {
+		type A = {
+			type: "a";
+			a: string;
+		};
+		type B = {
+			type: "b";
+			b: number;
+		};
+
+		type Result = Prettify<Normalize<A | B | null | undefined>>;
+
+		type ExpectedA = {
+			type: "a";
+			a: string;
+			b?: never;
+		};
+		type ExpectedB = {
+			type: "b";
+			a?: never;
+			b: number;
+		};
+
+		expectTypeOf<Result>().toEqualTypeOf<
+			ExpectedA | ExpectedB | null | undefined
+		>();
+	});
+
+	it("should preserve string primitives", () => {
+		type Primitive = "hi";
+
+		type Result = Normalize<Primitive>;
+
+		expectTypeOf<Result>().toEqualTypeOf<Primitive & Record<never, never>>();
+	});
+
+	it("should preserve number primitives", () => {
+		type Primitive = 12;
+
+		type Result = Normalize<Primitive>;
+
+		expectTypeOf<Result>().toEqualTypeOf<Primitive & Record<never, never>>();
+	});
+
+	it("should preserve boolean primitives", () => {
+		type Primitive = true;
+
+		type Result = Normalize<Primitive>;
+
+		expectTypeOf<Result>().toEqualTypeOf<Primitive & Record<never, never>>();
+	});
+
+	it("should preserve functions", () => {
+		type Func = (value: string) => Date;
+
+		type Result = Normalize<Func>;
+
+		expectTypeOf<Result>().toEqualTypeOf<Func & Record<never, never>>();
+	});
+
 	it("should preserve never", () => {
 		expectTypeOf<Normalize<never>>().toEqualTypeOf<never>();
+	});
+
+	it("should not preserve unknown", () => {
+		expectTypeOf<Normalize<unknown>>().toEqualTypeOf<{}>();
 	});
 
 	it("should error when passing a key not present in UnionKey<T>", () => {
