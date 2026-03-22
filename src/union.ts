87998015e54eb5baa1e191848cb9c105769cc55d
razhayat <razh.graffiti@gmail.com>
2026-03-22 15:38:14 +0200

    Made normalize support null and undefined

--- src/union.ts
+++ src/union.ts
@@ -1,8 +1,10 @@
 export type UnionKey<T> = T extends T ? keyof T : never;
 
-export type Normalize<T, K extends UnionKey<T> = UnionKey<T>> = T extends T
-	? T & Partial<Record<Exclude<K, keyof T>, never>>
-	: never;
+export type Normalize<T, K extends UnionKey<T> = UnionKey<T>> = T extends
+	| null
+	| undefined
+	? T
+	: T & Partial<Record<Exclude<K, keyof T>, never>>;
 
 export type Unbind<T> = Omit<Normalize<T>, never>;
 
