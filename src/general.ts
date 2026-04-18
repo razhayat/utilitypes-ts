2de987dd42b15c48631613f4fa56503e28136cb5
razhayat <razh.graffiti@gmail.com>
2026-04-18 20:06:33 +0300

    Added JsDocs to ValueOf

--- src/general.ts
+++ src/general.ts
@@ -1,6 +1,34 @@
 import { CustomTypeError } from "./error";
 import { Normalize, UnionKey } from "./union";
 
+/**
+ * Extracts the value types of an object `T`.
+ *
+ * By default, it returns a union of all property values.
+ * You can optionally provide a subset of keys `K` to extract
+ * only specific value types.
+ *
+ * @template T - The target object type
+ * @template K - Keys of `T` to extract values from (defaults to all keys)
+ *
+ * @example
+ * type Result = ValueOf<{ a: string; b: number }>;
+ * //   ^?
+ * // string | number
+ *
+ * @example
+ * type Result = ValueOf<{ a: string; b: number; c: boolean }, "a" | "c">;
+ * //   ^?
+ * // string | boolean
+ *
+ * @example
+ * type Result = ValueOf<
+ *   | { a: string }
+ *   | { b: number }
+ * >;
+ * //   ^?
+ * // string | number | undefined
+ */
 export type ValueOf<T, K extends UnionKey<T> = UnionKey<T>> = Normalize<T>[K];
 
 export type Suggestion<Options extends Base, Base = string> =
